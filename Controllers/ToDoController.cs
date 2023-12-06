75f08f7ed2769312d811da528ab972f308b56363
est_jelesbao <josenetu32@gmail.com>
2023-12-06 16:32:23 -0300

    Version 1.1

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MeuToDo.Data;
+using MeuToDo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace MeuToDo.Controllers
@@ -12,6 +15,17 @@ namespace MeuToDo.Controllers
     [Route(template: "v1")]
     public class ToDoController : ControllerBase
     {
+        [HttpGet]
+        [Route(template: "todos")]
+        public async Task<IActionResult> Get
+        (
+            [FromServices] AppDbContext context
+        )
+        {
+            var listTodos = await context.Todos.FirstOrDefaultAsync();
 
+            if (listTodos is null) return NotFound("List empty");
+            return Ok(listTodos);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,34 +1,27 @@
+
+
+using MeuToDo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 
-app.UseDeveloperExceptionPage();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseRouting();
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapGet("/", async context =>
-    {
-        await context.Response.WriteAsync("Hello World!");
-    });
-
-    endpoints.MapGet("/rotateste", async context =>
-    {
-        var configuration = context.RequestServices.GetRequiredService<IConfiguration>();
-        var mensagem = configuration["mensagem"];
-        await context.Response.WriteAsync(mensagem);
-    });
-});
-
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/rotateste", (IConfiguration configuration) =>
-{
-    return Results.Ok(configuration["mensagem"]);
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
 });
 
 app.Run();
